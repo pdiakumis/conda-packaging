40b3cec6794832528b7fd625570d8c8c03efffa7
pdiakumis <pdiakumis@unimelb.edu.au>
2019-04-03 18:41:03 +1100

    use channels when building

--- build_all.sh
+++ build_all.sh
@@ -6,14 +6,14 @@ NAME=$1
 echo "Building $NAME";
 echo "PATH is $PATH"
 
-#CHANNELS="-c pdiakumis -c conda-forge -c bioconda -c defaults"
+CHANNELS="-c pdiakumis -c conda-forge -c bioconda -c defaults"
 #PACKAGE_PATH=$(conda build $NAME $CHANNELS --output | tail -n1)
 PACKAGE_PATH=$(conda build $NAME --output | tail -n1)
 if [ -f $PACKAGE_PATH ] ; then
     echo "$PACKAGE_PATH exists, skipping"
 else
     echo "Building $PACKAGE_PATH"
-    conda build $NAME
+    conda build $NAME $CHANNELS
     anaconda upload $PACKAGE_PATH
     BASEDIR=$(dirname $(dirname $PACKAGE_PATH))
     FILENAME=$(basename $PACKAGE_PATH)
